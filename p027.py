bef452c9530ca4ccc1dbea73a0c9db7df5601c0d
Kayvan Sylvan <kayvan@sylvan.com>
2022-12-15 09:25:24 +0000

    Quadratic primes - using new primes module

---
+++ p027.py
@@ -0,0 +1,66 @@
+# Quadratic primes
+#
+# Euler discovered the remarkable quadratic formula:
+#
+# n^2 + n + 41
+#
+# It turns out that the formula will produce 40 primes for the consecutive integer values
+# 0 <= n <= 39. However, when n=40, n^2+n+41 is divisible by 41, and certainly when  n=41,
+# the result is clearly divisible by 41.
+#
+# The incredible formula n^2 - 79n + 1601 was discovered, which produces 80 primes for the 
+# consecutive values 0 <= n <= 79. The product of the coefficients, −79 and 1601, is −126479.
+#
+# Considering quadratics of the form:
+#
+# n^2 + an + b
+#
+# where abs(a)<1000 and abs(b)<=1000
+#
+# Find the product of the coefficients, a, b for the quadratic expression that produces the
+# maximum number of primes for consecutive values of n starting with n=0
+#
+
+from itertools import count
+from primes import is_prime
+from functools import cache
+from typing import Tuple
+
+def quadratic(a: int, b: int, n: int) -> int:
+    return n**2 + a*n + b
+
+def num_of_primes(a: int, b:int) -> int:
+    for i in count(0):
+        if not is_prime(quadratic(a, b, i)):
+            break
+    return i
+
+@cache
+def find_a_b(N: int) -> Tuple[int, int]:
+    """
+    Find a and b between -N to N+1 that maximize the sequence of primes.
+    
+    >>> find_a_b(10)
+    (-3, 7)
+    >>> find_a_b(100)
+    (-15, 97)
+    >>> find_a_b(1000)
+    (-61, 971)
+    """
+    t = (-1, 0, 0)
+    for i in range(-N, N+1):
+        if i % 2 == 0:
+            continue
+        for j in range(-N, N+1):
+            if not is_prime(j):
+                continue
+            k = (num_of_primes(i, j), i, j)
+            # if k[0] > 30: print(f"n^2 + {i}n + {j} -> {k[0]}")
+            t = max(t, k)
+    return (t[1], t[2])
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+    a, b = find_a_b(1000)
+    print (f"Project Euler Problem 27 answer: {a*b}")
---
+++ primes.py
@@ -0,0 +1,106 @@
+# Prime related utility methods.
+#
+
+from itertools import count
+from typing import List
+
+__p = [2, 3, 5]
+
+def next_prime(from_start: bool = False) -> int:
+    """
+    Generator that yields primes.
+    
+    If from_start=True, start from the beginning, else, yield the next prime.
+    
+    >>> p = next_prime(from_start = True)
+    >>> [next(p) for _ in range(5)]
+    [2, 3, 5, 7, 11]
+    >>> [next(p) for _ in range(2)]
+    [13, 17]
+    >>> p2 = next_prime()
+    >>> [next(p2) for _ in range(3)]
+    [19, 23, 29]
+    """
+    if from_start:
+        for p in __p:
+            yield p
+    p = __p[-1] + 2
+    while True:
+        is_prime = True
+        for i in count(1):
+            if p % __p[i] == 0:
+                is_prime = False
+                break
+            if __p[i] * __p[i] > p:
+                break
+        if is_prime:
+            __p.append(p)
+            yield p
+        p += 2
+
+def is_prime(n: int) -> bool:
+    """
+    Returns True if n is a prime number, False otherwise.
+    
+    >>> is_prime(2)
+    True
+    >>> is_prime(3)
+    True
+    >>> is_prime(0)
+    False
+    >>> is_prime(1)
+    False
+    >>> is_prime(4)
+    False
+    >>> is_prime(41)
+    True
+    """
+    if n < 2:
+        return False
+    p = next_prime(from_start = True)
+    for v in p:
+        if n == v:
+            return True
+        if n % v == 0:
+            return False
+        if v*v > n:
+            break
+    return True
+
+def factors(n: int) -> List[int]:
+    """
+    Returns a list of all prime numbers of n.
+    
+    >>> factors(2)
+    [2]
+    >>> factors(3)
+    [3]
+    >>> factors(0)
+    []
+    >>> factors(1)
+    []
+    >>> factors(4)
+    [2, 2]
+    >>> factors(44)
+    [2, 2, 11]
+    """
+    res = []
+    if n < 2:
+        return []
+    p = next_prime(from_start = True)
+    while n > 1:
+        for v in p:
+            while n % v == 0:
+                res.append(v)
+                n //= v
+            if v*v > n:
+                break
+        if v*v>n:
+            break
+    if n > 1:
+        res.append(n)
+    return res
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
